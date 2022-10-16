cb13731c084eeb1cb8bf5e5115a2f7653f1136e8
Andrey <trusov1967@gmail.com>
2022-10-16 18:43:18 +0300

    We add new functions

--- Desktop/Git/ex1.java
+++ Desktop/Git/ex1.java
@@ -1,15 +1,19 @@
 public class ex1 {
     public static void main(String[] args) {
-        int a=23;
-        int b=11;
-        System.out.println(a-b);
-        if ((a-b)<10) {
+        int a = 23;
+        int b = 13;
+        System.out.println(a - b);
+        if ((a - b) < 10) {
             System.out.println(" It is arror");
         }
-        else{
+        if ((a - b > 10)) {
             System.out.println("It is very good");
+        } 
+        if ((a-b)==10) {
+            System.out.println("URA!!!!!!!!");
         }
 
         
+
     }
 }

810399881c6b8c22f750ea4bf03c3af11806c1cd
Andrey <trusov1967@gmail.com>
2022-10-16 18:12:35 +0300

    We add new file

---
+++ Desktop/Git/ex1.java
@@ -0,0 +1,15 @@
+public class ex1 {
+    public static void main(String[] args) {
+        int a=23;
+        int b=11;
+        System.out.println(a-b);
+        if ((a-b)<10) {
+            System.out.println(" It is arror");
+        }
+        else{
+            System.out.println("It is very good");
+        }
+
+        
+    }
+}
